6abe98270aff43b9615b81db504029ce67360fa3
Pete Wildsmith <pete@dabapps.com>
2017-06-02 12:59:18 +0100

    debug output

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e
+set -x
 
-export NOMAD_ADDR=$PLUGIN_NOMAD_ADDRESS
+export NOMAD_ADDR=${PLUGIN_NOMAD_ADDRESS}
+IMAGE_TAG=${PLUGIN_IMAGE_TAG:-latest}
 
-/bin/nomad run $PLUGIN_CONFIGURATION
+# substitute the desired tag
+sed "s/IMAGE_TAG/${IMAGE_TAG}/" ${PLUGIN_CONFIGURATION} > config-${IMAGE_TAG}.hcl
+
+/bin/nomad run config-${IMAGE_TAG}.hcl
